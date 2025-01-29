27a7d8de075a43f6056a502f4d2c7864c8e3d52b
wimaxvs <wimaxvs.system@gmail.com>
2025-01-29 13:33:35 +0100

    Update page.tsx

--- app/pulpit/staty/page.tsx
+++ app/pulpit/staty/page.tsx
@@ -9,7 +9,7 @@ const page = async () => {
 
   return (
     <div
-      className={`profilPage w-full min-h-screen flex flex-row justify-center py-10 bg-[url('/images/statystyki_firmy.jpg')] bg-no-repeat bg-cover bg-left-bottom overflow-y-scroll`}
+      className={`profilPage w-full min-h-screen flex flex-row justify-center py-10 bg-[url('/images/statystyki_firmy2.jpg')] bg-no-repeat bg-cover bg-left-bottom overflow-y-scroll`}
     >
       <section
         className={`formSection rounded-md bg-gradient-to-br from-gray-800 to-gray-950 w-[95%] md:min-w-[95%] md:w-4/5 lg:w-2/3 md:min-h-5/6 md:h-5/6 max-h-[600px] md:max-h-[700px] p-2 md:p-10 flex flex-col gap-3 border border-primary`}
